62d732ec664905bd154d65ddc936ccdfda28f357
luannutels <luannutels@gmail.com>
2022-03-29 19:39:52 -0300

    second commit

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; //template string
 
-//enquanto
-while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSom;
-
-  contador = contador + 1;
-
-  console.log(contador);
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
 }
